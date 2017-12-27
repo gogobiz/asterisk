707ff36235f483347a790418d41d292b81738535
Kevin Clark <klclark@gogoair.com>
2017-12-27 09:32:51 -0700

    [PATCH 04/05] GOGOBIZ: Add TCP/TLS monitoring for reconnecting with stale socket
    # Conflicts:
    #	main/tcptls.c

--- main/tcptls.c
+++ main/tcptls.c
@@ -721,14 +721,8 @@ struct ast_tcptls_session_instance *ast_tcptls_client_create(struct ast_tcptls_s
 		goto error;
 	}
 	tcptls_session->client = 1;
-	tcptls_session->stream = ast_iostream_from_fd(&fd);
-	if (!tcptls_session->stream) {
-		goto error;
-	}
-
-	/* From here on out, the iostream owns the accept_fd and it will take
-	 * care of closing it when the iostream is closed */
-
+	tcptls_session->stale = 0;
+	tcptls_session->fd = desc->accept_fd;
 	tcptls_session->parent = desc;
 	tcptls_session->parent->worker_fn = NULL;
 	ast_sockaddr_copy(&tcptls_session->remote_address,
